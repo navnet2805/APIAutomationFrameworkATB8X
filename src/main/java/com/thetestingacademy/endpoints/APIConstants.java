ef3d0d1e82764831769bab6bc334130d8fb0ca15
navnet2805 <naveen.rp.2805@gmail.com>
2025-02-16 16:51:58 +0530

    Initial commit for API Automation Framework

---
+++ src/main/java/com/thetestingacademy/endpoints/APIConstants.java
@@ -0,0 +1,12 @@
+package com.thetestingacademy.endpoints;
+
+public class APIConstants {
+
+    //For now we hardcoded it
+
+    public static String BASE_URL = "https://restful-booker.herokuapp.com";
+    public static String CREATE_UPDATE_BOOKING_URL = "/booking";
+    public static String AUTH_URL = "/auth";
+    public static String PING_URL = "/ping";
+
+}
---
+++ src/test/java/com/thetestingacademy/tests/sample/TestIntegrationSample.java
@@ -0,0 +1,49 @@
+package com.thetestingacademy.tests.sample;
+
+
+import io.qameta.allure.Description;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestIntegrationSample {
+
+    /*
+    Create a booking
+    get a booking
+    update a booking
+    delete a booking
+     */
+
+    @Test(groups = "qa", priority = 1)
+    @Description("Create a Booking")
+    public void CreateBooking()
+    {
+        Assert.assertTrue(true);
+    }
+
+    @Test(groups = "qa", priority = 2)
+    @Description("Get a Booking")
+    public void GetBooking()
+    {
+        Assert.assertTrue(true);
+    }
+
+    @Test(groups = "qa", priority = 3)
+    @Description("Update a Booking")
+    public void UpdateBooking()
+    {
+        Assert.assertTrue(true);
+    }
+
+    @Test(groups = "qa", priority = 4)
+    @Description("Delete a Booking")
+    public void DeleteBooking()
+    {
+        Assert.assertTrue(true);
+    }
+
+
+
+
+
+}
